6058250863f29a83dae41fc10047b9861e7a5f84
Oanh <duongthuanvu@gmail.com>
2020-09-16 12:19:21 +0700

    fix directory

--- Divide_Block_Image/divide_block.py
+++ Divide_Block_Image/divide_block.py
@@ -4,14 +4,14 @@ import os
 
 current_dir = os.getcwd()
 print("current directory: ",current_dir)
-directory = '/home/oanh/Documents/Divide_Block_Image/divided_block'
+directory = current_dir+'/divided_block'
 #So luong anh trong file nguon
 n = 6
 #Cach chia block
 horizontal_divisor = 5
 vertical_divisor = 2
 for i in range(n):
-	path = '/home/oanh/Documents/Divide_Block_Image/image_source/image%d.jpg' %(i+1)
+	path = current_dir+'/image_source/image%d.jpg' %(i+1)
 	img_src = cv2.imread(path)
 	h, w, c = img_src.shape
 	horizontal_step = w//horizontal_divisor
@@ -22,7 +22,7 @@ for i in range(n):
 			img_sub = img_src[y*vertical_step:(y+1)*vertical_step, x*horizontal_step:(x+1)*horizontal_step,:]
 			print(img_sub.shape)
 			os.chdir(directory)
-			img_sub_name = 'image%d%d.jpg' %(i,y*horizontal_divisor+x)
+			img_sub_name = 'image%d%d.jpg' %(i+1,y*horizontal_divisor+x+1)
 			cv2.imwrite(img_sub_name,img_sub)
 			print('successfully saved ',img_sub_name)
 	os.chdir(current_dir)
